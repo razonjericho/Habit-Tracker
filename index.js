7c35ebcc5e698f819208bd8d85bae9814960a76b
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-04-07 03:09:54 +0800

    Add backend API: GET, POST, PATCH, DELETE for habits on server.js

--- index.js
+++ index.js
@@ -5,27 +5,63 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "habit_tracker",
+    password: "REMOVED",
+    port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
 let habits = [
     {id: 1, habit: "Wake up"},
     {id: 2, habit: "Pray"},
 ];
 
-app.get("/", (req, res) => {
+app.get("/habits", async (req, res) => {
     try {
+        const result = await db.query("SELECT * FROM habits");
+
+        res.json(habits);
 
     } catch (err) {
         console.log(err)
     }
 });
 
-app.post("/post", (req, res) => {
+app.post("/habits", async (req, res) => {
+    const newHabit = req.body.newHabit;
     try {
-
+        await db.query("INSERT INTO habits (habit) VALUES ($1)", [newHabit]);
+        res.redirect("/");
     } catch (err) {
-        console.log(err)
+        console.log(err);
+    }
+});
+
+app.patch("/habits/:id", async (req, res) => {
+    const editHabit = req.body.updatedHabit;
+    const id = req.body.updatedHabitId;
+    try {
+        await db.query("UPDATE habits SET habit = ($1) WHERE id = ($2)", [editHabit, id])
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.delete("/habits/:id", async (req, res) => {
+    const id = req.body.deletedHabitId
+
+    try {
+        await db.query("DELETE FROM habits WHERE id = $1", [id]);
+    } catch (err) {
+        console.log(err);
     }
 });
 
---
+++ server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "habit_tracker",
+    password: "REMOVED",
+    port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+let habits = [
+    {id: 1, habit: "Wake up"},
+    {id: 2, habit: "Pray"},
+];
+
+app.get("/habits", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM habits");
+        habits = result.rows;
+        res.json(habits);
+
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+app.post("/habits", async (req, res) => {
+    const addHabit = req.body.addHabit;
+    try {
+        const result = await db.query("INSERT INTO habits (habit) VALUES ($1) RETURNING *;", [addHabit]);
+        const newHabit = result.rows[0];
+        res.json(newHabit)
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.patch("/habits/:id", async (req, res) => {
+    const editHabit = req.body.editHabit;
+    const id = req.params.id;
+    try {
+        const result = await db.query("UPDATE habits SET habit = ($1) WHERE id = ($2) RETURNING *;", [editHabit, id])
+        const updatedHabit = result.rows[0];
+        res.json({ message: "Habit updated successfully", updatedHabit: updatedHabit });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.delete("/habits/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await db.query("DELETE FROM habits WHERE id = $1", [id]);
+        const rowCount = result.rowCount;
+        if (rowCount === 0) {
+            res.status(404).json({ error: "Habit not found" })
+        } else if (rowCount !== 0) {
+            res.json({ message: "Habit deleted successfully", id: id });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
