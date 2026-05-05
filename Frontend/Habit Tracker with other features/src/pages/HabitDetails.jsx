e0446631b379e5760016ad291655005687e96f95
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-05-06 00:52:38 +0800

    Fetch and display streak in Habit Details Page

--- Frontend/Habit Tracker with other features/src/pages/HabitDetails.jsx
+++ Frontend/Habit Tracker with other features/src/pages/HabitDetails.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import HabitList from "../components/HabitList/HabitList"
 import { useParams } from 'react-router-dom';
 import { HabitContext } from '../HabitContext';
 
@@ -7,6 +9,26 @@ function HabitDetailsPage(){
     const id = Number(params.id);
     const context = useContext(HabitContext);
     const { habits } = context;
+    const [ streaks, setStreak ] = useState(null);
+    const API_URL = "http://localhost:3000";
+
+    useEffect(() => {
+         const fetchStreak = async () => {
+                try {
+                   const response = await axios.get(`${API_URL}/habits/progress/${id}`);
+
+                setStreak(response.data);
+
+                } catch (err) {
+                    console.error('Error, unable to get habit streak:', err);
+                }
+            }
+                fetchStreak();
+    }, [id]);
+
+    if (streaks === null) {
+        return <p>loading...</p>
+    }
 
     const selectedHabit = habits.find(habit => habit.id === id);
 
@@ -14,11 +36,10 @@ function HabitDetailsPage(){
         return <p>Habit not found</p>;
     }
 
-    console.log(selectedHabit);
     return (
         <div>
             <h1>Habit Details</h1>
-            <p>Habit: {selectedHabit.habit}</p>
+            <p>Habit: {selectedHabit.habit} {streaks.streak} </p>          
         </div>
     )
 }
