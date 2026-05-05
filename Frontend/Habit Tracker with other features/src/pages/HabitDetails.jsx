79ff505a6e176d6df21682a13fa31b9bdbef84e0
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-05-06 00:22:55 +0800

    Render habit name on the Habit Details Page

--- Frontend/Habit Tracker with other features/src/pages/HabitDetails.jsx
+++ Frontend/Habit Tracker with other features/src/pages/HabitDetails.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { HabitContext } from '../HabitContext';
 
 function HabitDetailsPage(){
+    const params = useParams();
+    const id = Number(params.id);
+    const context = useContext(HabitContext);
+    const { habits } = context;
 
+    const selectedHabit = habits.find(habit => habit.id === id);
+
+    if (!selectedHabit) {
+        return <p>Habit not found</p>;
+    }
+
+    console.log(selectedHabit);
+    return (
+        <div>
+            <h1>Habit Details</h1>
+            <p>Habit: {selectedHabit.habit}</p>
+        </div>
+    )
 }
 
 export default HabitDetailsPage;
