794733f94b3dc14218239d66f18e16d80de8bbe8
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-05-18 18:59:44 +0800

    Replace archived habits state with habits state in Edit page

--- Frontend/Habit Tracker with other features/src/pages/Edit/Edit.jsx
+++ Frontend/Habit Tracker with other features/src/pages/Edit/Edit.jsx
@@ -7,26 +7,16 @@ import { HabitContext } from '../../HabitContext';
 function EditPage() {
     const context = useContext(HabitContext);
     const {habits, addHabit, editHabit, archiveHabit, restoreHabit, deleteHabit} = context;
-    const [archivedHabit, setArchivedHabits] = useState([]);
-    const API_URL = "http://localhost:3000";
-
-    useEffect(() => {
-    async function fetchArchivedHabits() {
-        const response = await axios.get(`${API_URL}/habits/edit/archive`);
-
-        setArchivedHabits(response.data);
-        }
-
-        fetchArchivedHabits();
-    }, []);
+    const activeHabits = habits.filter(habit => habit.active);
+    const archivedHabits = habits.filter(habit => !habit.active);
 
     return (
         <div>
             <h2>Edit Habits</h2>
-            <HabitList habits={habits} onEdit={editHabit} onArchive={archiveHabit} />
+            <HabitList habits={activeHabits} onEdit={editHabit} onArchive={archiveHabit} />
             <HabitInput onAdd={addHabit} />
             <h2>Archived Habits</h2>
-            <HabitList habits={archivedHabit} onRestore={restoreHabit} onDelete={deleteHabit} />
+            <HabitList habits={archivedHabits} onRestore={restoreHabit} onDelete={deleteHabit} />
         </div>
     )
     
