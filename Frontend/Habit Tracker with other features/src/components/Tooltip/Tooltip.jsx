15f3d8ccd0f739a342bf4394710097eff599b5bd
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-06-16 00:57:43 +0800

    refactor: extract tooltip logic from Progress Page to Tooltip component

--- Frontend/Habit Tracker with other features/src/components/Tooltip/Tooltip.jsx
+++ Frontend/Habit Tracker with other features/src/components/Tooltip/Tooltip.jsx
@@ -1,1 +1,100 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
+function Tooltip () {
+    const [ selectedDay, setSelectedDay ] = useState(null);
+    const [ tooltipPosition, setTooltipPosition ] = useState(null);
+    const [ clampedLeft, setClampedLeft ] = useState(null);
+
+    
+    const tooltipRef = useRef(null);
+    const calendarRef = useRef(null);
+       
+    
+    useEffect(() => {
+        if (!selectedDay || !tooltipRef.current || !tooltipPosition) return;
+
+        const size = tooltipRef.current.getBoundingClientRect();
+
+        const tooltipWidth = size.width;
+        const viewportWidth = window.innerWidth;
+
+        const centerX = tooltipPosition.left + (tooltipPosition.width / 2);
+
+        const idealLeft = centerX - (tooltipWidth / 2);
+
+        const padding = 8;
+
+        const minLeft = padding;
+
+        const maxLeft = viewportWidth - tooltipWidth - padding;
+
+        const safeLeft =
+            Math.min(
+                Math.max(idealLeft, minLeft),
+                maxLeft
+            );
+
+        setClampedLeft(safeLeft);
+
+    }, [selectedDay, tooltipPosition]);
+
+    function handleSelectDay(day, position){
+        if (selectedDay && selectedDay.day === day.day) {
+            setSelectedDay(null);
+            setTooltipPosition(null);
+        } else {
+            setSelectedDay(day);
+            setTooltipPosition(position);
+        }
+    }
+
+    function closeSelectedDay(){
+        setSelectedDay(null);
+        setTooltipPosition(null);
+    }
+
+    useEffect(() => {
+            if (!selectedDay) return;
+    
+                function handleDocumentClick(event) {
+                    if(tooltipRef.current?.contains(event.target)) {
+                        return;
+                    }
+    
+                    if(calendarRef.current?.contains(event.target)) {
+                        return;
+                    }
+                    closeSelectedDay();
+                }
+    
+            document.addEventListener("click", handleDocumentClick);
+    
+            return () => {
+                document.removeEventListener("click", handleDocumentClick);
+            }
+    
+        }, [selectedDay]);
+
+
+    return (
+        <div>
+            {selectedDay && (
+                <div 
+                className="tooltip"
+                ref={tooltipRef}
+                style={{
+                    top: tooltipPosition.top - 95,
+                    left: clampedLeft
+                }}
+                >
+                    <p>{selectedDay.day}</p>
+                    <p>{selectedDay.completed} / {selectedDay.totalHabits}</p>
+                    <p>{Math.round(selectedDay.intensity * 100)}% Complete</p>
+
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Tooltip;
--- Frontend/Habit Tracker with other features/src/pages/Progress/Progress.jsx
+++ Frontend/Habit Tracker with other features/src/pages/Progress/Progress.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import HabitList from "../../components/HabitList/HabitList";
 import Calendar from '../../services/Calendar/Calendar';
@@ -12,9 +12,6 @@ function ProgressPage(props) {
     const { habits } = context;
     const [ streaks, setStreak ] = useState({});
     const [ heatMap, setHeatMap ] = useState({});
-    const [ selectedDay, setSelectedDay ] = useState(null);
-    const [ tooltipPosition, setTooltipPosition ] = useState(null);
-    const [ clampedLeft, setClampedLeft ] = useState(null);
     const activeHabits = habits.filter(habit => habit.active === true);
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -68,74 +65,6 @@ function ProgressPage(props) {
         navigate(`/progress/day/${date}`);
     }
 
-    function handleSelectDay(day, position){
-        if (selectedDay && selectedDay.day === day.day) {
-            setSelectedDay(null);
-            setTooltipPosition(null);
-        } else {
-            setSelectedDay(day);
-            setTooltipPosition(position);
-        }
-    }
-
-    function closeSelectedDay(){
-        setSelectedDay(null);
-        setTooltipPosition(null);
-    }
-
-    const tooltipRef = useRef(null);
-    const calendarRef = useRef(null);
-   
-
-    useEffect(() => {
-        if (!selectedDay || !tooltipRef.current || !tooltipPosition) return;
-
-        const size = tooltipRef.current.getBoundingClientRect();
-
-        const tooltipWidth = size.width;
-        const viewportWidth = window.innerWidth;
-
-        const centerX = tooltipPosition.left + (tooltipPosition.width / 2);
-
-        const idealLeft = centerX - (tooltipWidth / 2);
-
-        const padding = 8;
-
-        const minLeft = padding;
-
-        const maxLeft = viewportWidth - tooltipWidth - padding;
-
-        const safeLeft =
-            Math.min(
-                Math.max(idealLeft, minLeft),
-                maxLeft
-            );
-
-        setClampedLeft(safeLeft);
-
-    }, [selectedDay, tooltipPosition]);
-
-    useEffect(() => {
-        if (!selectedDay) return;
-
-            function handleDocumentClick(event) {
-                if(tooltipRef.current?.contains(event.target)) {
-                    return;
-                }
-
-                if(calendarRef.current?.contains(event.target)) {
-                    return;
-                }
-                closeSelectedDay();
-            }
-
-        document.addEventListener("click", handleDocumentClick);
-
-        return () => {
-            document.removeEventListener("click", handleDocumentClick);
-        }
-
-    }, [selectedDay]);
    
     return (
         <div>
@@ -143,35 +72,13 @@ function ProgressPage(props) {
             <button onClick= {props.previous} > Previous </button>
             <h2>{months[props.month]} {props.year} </h2>
             <button onClick= {props.next} > Next </button>
-            <div ref={calendarRef} >
+            <div>
                 <Calendar
-                month={props.month}
-                year={props.year}
-                heatMap={heatMap}
-                onSelectedDay={handleSelectDay}
-                onCloseSelectedDay={closeSelectedDay}
-            />
+                    month={props.month}
+                    year={props.year}
+                    heatMap={heatMap}
+                />
             </div>
-            
-
-            {selectedDay && (
-                <div 
-                className="tooltip"
-                ref={tooltipRef}
-                style={{
-                    top: tooltipPosition.top - 95,
-                    left: clampedLeft
-                }}
-                >
-                    <p>{selectedDay.day}</p>
-                    <p>{selectedDay.completed} / {selectedDay.totalHabits}</p>
-                    <p>{Math.round(selectedDay.intensity * 100)}% Complete</p>
-
-                        <button onClick={() => {viewDayDetails(selectedDay.day)} }>
-                            View more details
-                        </button>
-                </div>
-            )}
 
             <HabitList
                 habits={activeHabits}
