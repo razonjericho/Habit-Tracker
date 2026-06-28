3c4103c9afb090b089753be1fd80b109bda4e9ee
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-06-28 23:04:17 +0800

    feat: add logout function

--- Frontend/Habit Tracker with other features/src/components/Header/Header.jsx
+++ Frontend/Habit Tracker with other features/src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Header.css'
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
+
+    function logOut() {
+        
+        console.log("Logout clicked");
+        localStorage.removeItem("token");
+        console.log(localStorage.getItem("token"));
+        navigate(`/auth/login`);
+        
+    }
+
+
     return (
         <header>
                 <h1>Habit Tracker</h1>
@@ -13,9 +26,8 @@ function Header() {
                     <li><Link to="/progress">Progress</Link></li>
                 </ul>      
             </nav>
+            <button onClick={logOut} > Log Out </button>
         </header>
-        
-        
     );
 }   
 
