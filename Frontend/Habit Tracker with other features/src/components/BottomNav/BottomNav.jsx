73fe4afa03f4f6cdc4fdd71a7375f0e8380e1578
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-07-10 00:55:54 +0800

    feat: add Material UI bottom navigation with icons

--- Frontend/Habit Tracker with other features/src/components/BottomNav/BottomNav.jsx
+++ Frontend/Habit Tracker with other features/src/components/BottomNav/BottomNav.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { BottomNavigationAction, BottomNavigation } from "@mui/material"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined"
+import InsightsOutlinedIcon from "@mui/icons-material/InsightsOutlined"
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
+
 
 function BottomNav() {
     return (  
@@ -17,9 +21,9 @@ function BottomNav() {
                         width: "100%",
                     }}
                 >
-                    <BottomNavigationAction label="Home" component={RouterLink} to="/" />
-                    <BottomNavigationAction label="Progress" component={RouterLink} to="/progress" />  
-                    <BottomNavigationAction label="Edit" component={RouterLink} to="/edit" />
+                    <BottomNavigationAction label="Home" icon={<HomeOutlinedIcon />} component={RouterLink} to="/" />
+                    <BottomNavigationAction label="Progress" icon={<InsightsOutlinedIcon />} component={RouterLink} to="/progress" />  
+                    <BottomNavigationAction label="Edit" icon={<EditOutlinedIcon />} component={RouterLink} to="/edit" />
                 </BottomNavigation>
             
     )
