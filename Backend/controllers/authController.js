55fb8920c6c3908cd801a8cdaf922df54fc97098
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-07-24 05:54:11 +0800

    fix: add Vercel rewrite for React Router

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -79,10 +79,6 @@ const logUser = async (req, res) => {
 
         
     } catch (err) {
-        console.error("========== LOGIN ERROR ==========");
-        console.dir(err, { depth: null });
-        console.error(err.stack);
-
         console.error(err);
         res.status(500).json({ error: "Failed to log the user" });
     }
